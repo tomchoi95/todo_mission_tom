5f6f0d1f417ebfd93ef12ef9bf5862e1bca49297
Tom Bum Su Choi <qjatn0545@naver.com>
2025-02-14 15:13:59 +0900

    할 일 목록 화면을 위한 TodoListView 클래스 구현 및 상태 관리 개선

--- lib/views/todo_list.dart
+++ lib/views/todo_list.dart
@@ -1,45 +1,45 @@
 import 'package:flutter/material.dart';
-import '../viewmodels/todo_view_model.dart';
+import 'package:todo_mission_ton/viewmodels/todo_view_model.dart';
 
-// TodoListScreen 클래스는 할 일 목록 화면을 나타냅니다.
-class TodoListScreen extends StatefulWidget {
-  @override
-  _TodoListScreenState createState() => _TodoListScreenState();
-}
-
-// _TodoListScreenState 클래스는 TodoListScreen의 상태를 관리합니다.
-class _TodoListScreenState extends State<TodoListScreen> {
-  final TodoViewModel _todoViewModel = TodoViewModel(); // TodoViewModel 인스턴스 사용
-
-  @override
-  void initState() {
-    super.initState();
-    _todoViewModel.addListener(_update);
-  }
-
-  @override
-  void dispose() {
-    _todoViewModel.removeListener(_update);
-    super.dispose();
-  }
-
-  void _update() {
-    setState(() {});
-  }
+class TodoListView extends StatelessWidget {
+  const TodoListView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final VM = TodoListViewModel();
+
     return Scaffold(
       appBar: AppBar(title: Text('Todo List')),
-      body: _todoViewModel.buildTodoList(), // 할 일 목록을 화면에 표시
-      floatingActionButton: FloatingActionButton(
-        onPressed:
-            () => _todoViewModel.promptAddTodoItem(
-              context,
-            ), // 플로팅 액션 버튼을 눌렀을 때 할 일 추가 입력 창을 표시
-        tooltip: 'Add task',
-        child: Icon(Icons.add),
+      body: _buildTodoList(),
+      floatingActionButton: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.plus_one),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+
+  Widget _buildTodoList() {
+    return ListView.builder(
+      itemCount: 4,
+      itemBuilder: (context, index) {
+        return _todoListRow(index);
+      },
+    );
+  }
+
+  Widget _todoListRow(int index) {
+    final int indes = index;
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () {
+            // 완료 상태 토글 버튼 구현하기.
+          },
+          icon: Icon(Icons.star),
+        ),
+        Text('타이틀 들어갈 곳'),
+      ],
     );
   }
 }
